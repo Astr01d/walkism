24eeac47271f3d0a025275c5682b5465a3a19c40
Astr01d <32207106+Astr01d@users.noreply.github.com>
2024-03-17 18:28:20 +0100

    removing pause menu shit

--- Main/PauseMenu/PauseMenu.gd
+++ Main/PauseMenu/PauseMenu.gd
@@ -2,17 +2,16 @@ extends Node2D
 
 
 
-func pause_unpause():
-	var new_pause_state = !get_tree().paused
-	get_tree().paused = new_pause_state
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	self.hide()
-	pass
-
 func pause():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	$VBoxContainer/Resume.grab_focus()
 
 
 func _on_button_pressed():
-	pause_unpause()
+	
+	pass
+
+
+func _on_quit_pressed():
+	OS.alert("meow","meow")
+	pass
--- Main/game_manager.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-#horrible game manager singleton 
-#nothing in this yet bc i decided to not use this for the pause menu after all 
