4b9b90c0a55b755f97b3be769fd95a4e6a607b6a
Astr01d <32207106+Astr01d@users.noreply.github.com>
2024-03-22 23:56:55 +0100

    more controller stuff

--- Main/Player.gd
+++ Main/Player.gd
@@ -12,6 +12,7 @@ const SENSITIVITY = .15
 var game_gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var gravity = 0 
 var direction : Vector3
+var keyboard_input : Vector2
 
 var mouse_input : Vector2
 
@@ -26,7 +27,7 @@ func _ready():
 	pass
 
 func _input(event):
-	
+	#	mouse viewing stuff
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		mouse_input.x += event.relative.y * .015
 		mouse_input.y += -event.relative.x * .015
@@ -35,46 +36,58 @@ func _input(event):
 
 
 func _process(delta):
-	
+#	gravity stuff
 	if not is_on_floor():
 		gravity = lerp(game_gravity, game_gravity/2, Input.get_action_strength("ui_accept"))
 		velocity.y -= gravity * delta
-	
+#	jump stuff
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+#	controller viewing stuff
 	var controller_input_r : Vector2
 	controller_input_r.y = Input.get_axis("Joypad_R_Right","Joypad_R_Left") 
 	controller_input_r.x = Input.get_axis("Joypad_R_Up","Joypad_R_Down")
-	
+#	combining mouse n controller meow
 	var head_rot = controller_input_r + mouse_input 
-	
+#	applying head and cam rotation
 	self.transform = self.transform.rotated_local(Vector3.UP, head_rot.y * SENSITIVITY)
 	$Head.transform = $Head.transform.rotated_local(Vector3.LEFT, head_rot.x * SENSITIVITY)
-	
+#	resetting the mouse input
 	mouse_input = Vector2.ZERO
-	
+#	clamping head rotation meow
 	$Head.rotation.x = clamp($Head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir : Vector2
-	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if input_dir:
-		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir : Vector2
+	var controller_input : Vector2
+
+	var target_kb_input : Vector2
+	
+#	read controller input
+	controller_input = Input.get_vector("Joypad_L_Left", "Joypad_L_Right", "Joypad_L_Up", "Joypad_L_Down")
+#	read keyboard input
+	target_kb_input = Input.get_vector("Keyboard_L_Left", "Keyboard_L_Right", "Keyboard_L_Up", "Keyboard_L_Down")
+	
+	keyboard_input = lerp(keyboard_input, target_kb_input, .3)
+
+
+	input_dir = controller_input.limit_length(1.0) + keyboard_input.limit_length(1.0)
+	
+	print(input_dir)
+	
+	if (target_kb_input or controller_input):
+		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+		
 	
 	speed = lerp(PLAYER_SPEED, PLAYER_SPEED * sprint, Input.get_action_strength("sprint_button"))
 	
-	if input_dir:
-		acceleration = move_toward(acceleration, speed, 2)
+	if (target_kb_input or controller_input):
+		acceleration = move_toward(acceleration, speed, .9)
 	else:
 		acceleration = move_toward(acceleration, 0, .7)
 		pass
 	
-	
 	velocity.x = direction.x * acceleration
 	velocity.z = direction.z * acceleration
-	print(direction)
 	
 	move_and_slide()
